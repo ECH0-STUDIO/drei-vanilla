4c9f5dbc4214a8b7d4c3b8544ed832edf9f14c92
ECH0-STUDIO <hello@ech0.work>
2025-06-08 19:55:40 +0700

    Add files via upload

---
+++ drei-vanilla-main/src/core/index.ts
@@ -0,0 +1,23 @@
+// Shaders
+export * from './pcss'
+export * from './Caustics'
+export * from './shaderMaterial'
+
+// Staging/Prototyping
+export * from './AccumulativeShadows'
+export * from './Cloud'
+
+// Misc
+export * from './useFBO'
+export * from './SpriteAnimator'
+
+// Abstractions
+export * from './Outlines'
+export * from './Billboard'
+export * from './Splat'
+
+// Gizmos
+export * from './Grid'
+
+// Portals
+export * from './MeshPortalMaterial'
